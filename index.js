a84681d515116254e774f19112974b5f3041879d
Sachin Tripathi <tripathisachinn@gmail.com>
2022-03-17 21:07:21 +0530

    UnderDevelopment

---
+++ index.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing;
+    console.log(this.dataset || '');
+    document.documentElement.style.setProperty(`${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
